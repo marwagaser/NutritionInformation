4995a977b90bec521945bdfc28cb0f306d7d5eaa
Marwa Gaser <marwagaser@gmail.com>
2020-01-18 18:44:33 +0200

    added error handling

--- src/app/calories-info/calories-info.component.ts
+++ src/app/calories-info/calories-info.component.ts
@@ -7,14 +7,29 @@ import { FetchFoodInfoService } from "../fetch-food-info.service";
   styleUrls: ["./calories-info.component.css"]
 })
 export class CaloriesInfoComponent implements OnInit {
-  public food;
-
+  public food = "";
+  public errorMessage = "";
+  searchWord = "tomatoe";
   constructor(private foodService: FetchFoodInfoService) {
     //executed before the ngOnInit
-    this.foodService.getConfig().subscribe(data => {
-      this.food = data["parsed"][0]["food"]["nutrients"];
-      console.log(data["parsed"][0]["food"]["nutrients"]);
-    });
   }
-  ngOnInit() {} //executed right after the construcor
+  ngOnInit() {
+    this.foodService.getConfig(this.searchWord).subscribe(
+      data => {
+        if (data["parsed"].length == 0) {
+          if (data["hints"].length == 0) {
+            this.errorMessage = "The food you're searching for is not found.";
+            console.log(this.errorMessage);
+          } else {
+            this.food = data["hints"][0]["food"]["nutrients"];
+          }
+        } else {
+          this.food = data["parsed"][0]["food"]["nutrients"];
+        }
+      },
+      error => {
+        this.errorMessage = error;
+      }
+    );
+  } //executed right after the construcor
 }
--- src/app/fetch-food-info.service.ts
+++ src/app/fetch-food-info.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 @Injectable({
   providedIn: "root"
 })
 export class FetchFoodInfoService {
   configUrl;
   constructor(private http: HttpClient) {}
-  getConfig() {
+  getConfig(searchWord) {
     this.configUrl =
-      "https://api.edamam.com/api/food-database/parser?ingr=tomatoe&app_id=620e1ffc&app_key=c736d1271d585ed32f23afb516bb1b28";
+      "https://api.edamam.com/api/food-database/parser?ingr=" +
+      searchWord +
+      "&app_id=620e1ffc&app_key=c736d1271d585ed32f23afb516bb1b28";
     console.log(this.http.get(this.configUrl));
-    return this.http.get(this.configUrl);
+    return this.http.get(this.configUrl).pipe(catchError(this.errorHandling));
+  }
+  errorHandling(error: HttpErrorResponse) {
+    return Observable.throw(error.message || "Server Error");
   }
 }
